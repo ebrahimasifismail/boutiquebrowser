338c8d963585e891b91df01f294b13de6aea8af0
ebrahimasifismail <ebrahimasifismail@gmail.com>
2019-05-18 20:51:42 +0530

    first commit

---
+++ textiles/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from textiles.models import Product, ProductImage, SubProduct, Order, Address
+# Register your models here.
+# class PaytmHistoryAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'MID', 'TXNAMOUNT', 'STATUS')
+
+
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(SubProduct)
+admin.site.register(Order)
+admin.site.register(Address)
+
+# admin.site.register(PaytmHistory, PaytmHistoryAdmin)
+# 
+
+
